b8d73a6c46e8f18f800e999f8cc73c9d71c3e46e
Andrzej Wójtowicz <andre@amu.edu.pl>
2020-09-15 17:16:00 +0200

    Fixes

--- code3/hidden.py
+++ code3/hidden.py
@@ -9,21 +9,28 @@
 #    c) wybierz "Hobbyist" > "Exploring the API" i wybierz "Next"
 #    d) uzupełnij formularz (może być wymagana weryfikacja numeru telefonu)
 #    e) w kolejnym kroku opisz po angielsku jak zamierzasz wykorzystać dane
-#       Twittera (możesz wspomnieć, że uczysz się Pythona, chcesz dowiedzieć
-#       się jak korzystać z API, pobrać oś czasu użytkownika oraz
-#       listę jego znajomych); w sekcji "Specifics" zaznacz wszystko
+#       Twittera (wspomnij, że uczysz się Pythona, chcesz dowiedzieć
+#       się jak korzystać z API Twittera, zamierzasz napisać dwa mini-projekty,
+#       które w interaktywny sposób pobierają nazwę konta Twittera i dla podanego
+#       konta w projekcie nr 1 pobierzesz i wyświetlisz oś czasu użytkownika
+#       zwróconą w formacie JSON, a w projekcie nr 2 pobierszesz jego listę jego
+#       znajomych w formacie JSON i przeparsujesz ją w celu wyciągnięcia informacji
+#       o znajomych; opiszę tę sekcję w miarę szczegółowo, gdyż wpływa ona
+#       na akceptację Twojego zgłoszenia); w sekcji "Specifics" zaznacz wszystko
 #       na "No"
 #    f) zweryfikuj zgłoszenie, przeczytaj warunki umowy i wyślij zgłoszenie
-#    g) potwierdz zgłoszenie klikając na link w wiadomości mailowej,
+#    g) potwierdź zgłoszenie klikając na link w wiadomości mailowej,
 #       którą otrzymasz po wysłaniu zgłoszenia
 # 3. Gdy Twoje zgłoszenie zostanie zaakceptowane, wejdź jeszcze raz
 #    na https://developer.twitter.com/en/apps i ponownie spróbuj
-#    utworzyć nową aplikację ("Create an app")
-# 4. Utwórz aplikację i podmień poniżej cztery ciągi znaków dotyczące
-#    consumer_key, consumer_secret, token_key i token_secret
+#    utworzyć nową aplikację ("Standalone Apps" > "Create App")
+# 4. Utwórz aplikację, w karcie "Keys and tokens" wygeneruj klucze i podmień
+#    poniżej cztery ciągi znaków dotyczące consumer_key, consumer_secret,
+#    token_key i token_secret (podmień wszystko co znajduje się między
+#    cudzysłowami, łącznie z nawiasami ostrokątnymi)
 
 def oauth():
-    return {"consumer_key":    "h7Lu...Ng",
-            "consumer_secret": "dNKenAC3New...mmn7Q",
-            "token_key":       "10185562-eibxCp9n2...P4GEQQOSGI",
-            "token_secret":    "H0ycCFemmC4wyf1...qoIpBo"}
+    return {"consumer_key":    "<tutaj umiesc Consumer Keys - API key>",
+            "consumer_secret": "<tutaj umiesc Consumer Keys - API key secret>",
+            "token_key":       "<tutaj umiesc Authentication Tokens - Access Token & Secret - Access token>",
+            "token_secret":    "<tutaj umiesc Authentication Tokens - Access Token & Secret - Access token secret>"}
