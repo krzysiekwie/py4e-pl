a56d6e8929386bdbca33497fa379ff597742b194
Andrzej Wójtowicz <andre-wojtowicz@users.noreply.github.com>
2020-09-29 15:44:56 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
 <?php
 
 $foot = '
+<p class="alert alert-info" role="alert">
+<b>Strona w budowie. Jeśli chcesz pomóc w tłumaczeniach, zajrzyj do repozytorium GitHub
+<a href="https://github.com/andre-wojtowicz/py4e-pl/blob/master/README.md"
+   target="_blank">https://github.com/andre-wojtowicz/py4e-pl</a>.</b>
+</p>
 <p style="font-size: 75%; margin-top: 5em;">
-Copyright Creative Commons Attribution 3.0 - Charles R. Severance
+Copyright Creative Commons Uznanie autorstwa 3.0 - Charles R. Severance
 </p>';
 
 $OUTPUT->setAppFooter($foot);
