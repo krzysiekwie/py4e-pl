f6c651f746b4b112547bfd534981ffca46973cb0
Andrzej Wójtowicz <andre@amu.edu.pl>
2020-09-30 22:55:16 +0200

    Book HTML Toc fixes

--- bookfoot.php
+++ bookfoot.php
@@ -31,22 +31,22 @@ function onSelect() {
 </script>    
 <div style="float:right">
 <select id="chapters" onchange="onSelect();">
-  <option <?= x_sel("01-intro") ?>>Rozdział 1: Wstęp</option>
-  <option <?= x_sel("02-variables") ?>>Rozdział 2: Zmienne, wyrażenia i instrukcje</option>
-  <option <?= x_sel("03-conditional") ?>>Rozdział 3: Wykonanie warunkowe</option>
-  <option <?= x_sel("04-functions") ?>>Rozdział 4: Funkcje</option>
-  <option <?= x_sel("05-iterations") ?>>Rozdział 5: Pętle i iteracje</option>
-  <option <?= x_sel("06-strings") ?>>Rozdział 6: Ciągi znaków</option>
-  <option <?= x_sel("07-files") ?>>Rozdział 7: Pliki</option>
-  <option <?= x_sel("08-lists") ?>>Rozdział 8: Listy</option>
-  <option <?= x_sel("09-dictionaries") ?>>Rozdział 9: Słowniki</option>
-  <option <?= x_sel("10-tuples") ?>>Rozdział 10: Krotki</option>
-  <option <?= x_sel("11-regex") ?>>Rozdział 11: Wyrażenia regularne</option>
-  <option <?= x_sel("12-network") ?>>Rozdział 12: Programy sieciowe</option>
-  <option <?= x_sel("13-web") ?>>Rozdział 13: Korzystanie z usług sieciowych</option>
-  <option <?= x_sel("14-objects") ?>>Rozdział 14: Programowanie obiektowe</option>
-  <option <?= x_sel("15-database") ?>>Rozdział 15: Bazy danych i SQL</option>
-  <option <?= x_sel("16-viz") ?>>Rozdział 16: Wizualizacja danych</option>
+  <option <?= x_sel("01-intro.php") ?>>Rozdział 1: Wstęp</option>
+  <option <?= x_sel("02-variables.php") ?>>Rozdział 2: Zmienne, wyrażenia i instrukcje</option>
+  <option <?= x_sel("03-conditional.php") ?>>Rozdział 3: Wykonanie warunkowe</option>
+  <option <?= x_sel("04-functions.php") ?>>Rozdział 4: Funkcje</option>
+  <option <?= x_sel("05-iterations.php") ?>>Rozdział 5: Pętle i iteracje</option>
+  <option <?= x_sel("06-strings.php") ?>>Rozdział 6: Ciągi znaków</option>
+  <option <?= x_sel("07-files.php") ?>>Rozdział 7: Pliki</option>
+  <option <?= x_sel("08-lists.php") ?>>Rozdział 8: Listy</option>
+  <option <?= x_sel("09-dictionaries.php") ?>>Rozdział 9: Słowniki</option>
+  <option <?= x_sel("10-tuples.php") ?>>Rozdział 10: Krotki</option>
+  <option <?= x_sel("11-regex.php") ?>>Rozdział 11: Wyrażenia regularne</option>
+  <option <?= x_sel("12-network.php") ?>>Rozdział 12: Programy sieciowe</option>
+  <option <?= x_sel("13-web.php") ?>>Rozdział 13: Korzystanie z usług sieciowych</option>
+  <option <?= x_sel("14-objects.php") ?>>Rozdział 14: Programowanie obiektowe</option>
+  <option <?= x_sel("15-database.php") ?>>Rozdział 15: Bazy danych i SQL</option>
+  <option <?= x_sel("16-viz.php") ?>>Rozdział 16: Wizualizacja danych</option>
 </select>
 </div>
 
