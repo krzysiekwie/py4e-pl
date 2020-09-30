afbbe00377b3e700bb7efbe94ec4f8cf813e35b5
Andrzej Wójtowicz <andre@amu.edu.pl>
2020-09-30 21:52:41 +0200

    Book description

--- book.php
+++ book.php
@@ -1,130 +1,32 @@
 <?php include("top.php"); ?>
 <?php include("nav.php");?>
-<div style="float: right; margin: 5px;"/><iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=drchu02-20&marketplace=amazon&region=US&placement=1530051126&asins=1530051126&linkId=2ea6c883c6cf11f29568856139bad34b&show_border=true&link_opens_in_new_window=true"></iframe></div>
-<h2>Python for Everybody</h2>
+<!-- <div style="float: right; margin: 5px;"/><iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=drchu02-20&marketplace=amazon&region=US&placement=1530051126&asins=1530051126&linkId=2ea6c883c6cf11f29568856139bad34b&show_border=true&link_opens_in_new_window=true"></iframe></div> -->
+<h2>Python dla wszystkich</h2>
 <p>
-The goal of this book is to provide an Informatics-oriented introduction to programming. The primary difference between a computer science approach and the Informatics approach taken in this book is a greater focus on using Python to solve data analysis problems common in the world of Informatics.
-The <a href="http://www.py4inf.com/">Python 2 version</a> of the book is still available.
-        </p>
-
-<p><b><a href="courses">Other courses / web sites using this book</a></b>
-</p>
-<p>
-Book translations:
+    Celem książki jest wprowadzenie do programowania, ze szczególnym uwzględnieniem wykorzystywania Pythona do rozwiązywania podstawowych problemów związanych analizą danych, które mogą pojawić się w codziennej pracy.
 </p>
 <ul>
-  <li>
-     English -
-<a href="https://amzn.to/29w0lsX" target="_blank">Python for Everbody: Exploring Data in Python3</a>
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Printed book on
-<a href="https://www.amazon.in/dp/9352136276" target="_blank">Amazon India</a>
-(low-cost shipping within India thanks to
-<a href="http://www.shroffpublishers.com/books/9789352136278/"
-   target="_blank">Shroff Publishing</a>)
-</li>
-       <li class="menu-text"><a href="http://amzn.to/29LrtC0" target="_blank">Kindle edition</a> of the book</li>
-       <li class="menu-text">Free: <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="html3">HTML</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.epub" target="_blank">EPUB</a></li>
-       <li class="menu-text"><a href="https://eng.libretexts.org/Textbook_Maps/Computer_Science/Map%3A_Python_for_Everybody_(Severance)" target="_blank">HTML with examples in Jupyter Notebooks from LibreTexts.org</a></li>
-<!--
-       <li class="menu-text"><a href="https://github.com/csev/py4e/raw/master/book3/zips/pfe.zip" target="_blank">Download HTML in a ZIP (~6MB)</a></li>
--->
-<!--
-       <li class="menu-text"><a href="trinket3" target="_blank">Interactive HTML from Trinket.io</a></li>
--->
-       <li class="menu-text"><a href="https://books.trinket.io/pfe/index.html" target="_blank">Interactive HTML from Trinket.io</a></li>
-     </ul>
-  </li>
-<li> Spanish -
-<a href="https://amzn.to/3axKzHv" target="_blank">Python para todos: Explorando la información con Python 3</a>
-<ul>
-<li>
-Translated book, autograders, resources, and web site at
-<a href="https://es.py4e.com/book">https://es.py4e.com</a>
-</li>
-<li>
-<a href="https://github.com/csev-es" target="_blank">Github repo</a> - 
-    Contributors:
-    <a href="https://github.com/don-charlie-pc" target="_blank">Juan Carlos Perez Castellanos</a>,
-    <a href="https://github.com/jdougnac" target="_blank">Juan Dougnac</a>,
-    <a href="https://github.com/danielmerino" target="_blank">Daniel Merino Echeverría</a>,
-    Jaime Bermeo Ramírez
-    and
-    <a href="https://github.com/ftardio" target="_new">Fernando Tardío</a>.
-</li>
-</ul>
-</li>
-<li> Italian
-<a href="https://amzn.to/2qGksK6" target="_blank">Python per tutti: Esplorare dati con Python3</a>
-     <ul class="menu vertical nested is-active">
-<li>
-       Free <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/IT_it/pythonlearn.epub" target="_blank">EPUB</a>
-</li>
-<li> 
-<a href="https://github.com/vittore/py4e/" target="_blank">Book source on github</a>, 
-(Thanks to
-<a href="https://www.linkedin.com/in/alessandrorossetti/" target="_blank">Alessandro Rossetti</a>
-and
-<a href="https://www.linkedin.com/in/vittorezen/" target="_blank">Vittore Zen</a>)
-</li>
-     </ul>
-  </li>
-  <li> Portuguese -
-<a href="https://amzn.to/2S6GpA6" target="_blank">Python Para Todos: Explorando Dados com Python 3</a>
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Free:
-        <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/PT_br/pythonlearn.epub" target="_blank">EPUB</a>
-        </li>
-        <li> <a href="https://github.com/ras-ufcg/py4e" target="_blank">Book source on github</a> and 
-<a href="https://github.com/ras-ufcg/py4e/blob/master/RAS_Team_Members.txt" target="_blank">translation team</a>
-(Thanks to <a href="https://twitter.com/yuriloia" target="_blank">Yuri Loia de Medeiros</a>).
-</li>
-  <li> Korean (commercial translation)
-<a href="http://www.yes24.com/Product/Goods/77401048?Acode=101" target="_blank">데이터를 다루며 배우는 파이썬</a>
-  </li>
-     </ul>
-  </li>
-  <li>
-     Chinese - In progress
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Free:
-
-
-        <a href="http://do1.dr-chuck.com/pythonlearn/ZH_cn/pythonlearn.pdf" target="_blank">PDF</a>,
-       <a href="http://do1.dr-chuck.com/pythonlearn/ZH_cn/pythonlearn.epub" target="_blank">EPUB</a>
-        </li>
-        <li> 
-<a href="https://github.com/RodenLuo/py4e-cn" target="_blank">Book source on github</a> -
-(Thanks to Deng Luo)
-</li>
-  <li> There is an in-China copy of the videos available at
-<a href="https://www.bilibili.com/video/av46649799" target="_new">
-https://www.bilibili.com/video/av46649799</a>
-  </li>
-     </ul>
-  </li>
-
+    <li>
+        Polskie tłumaczenie:
+        <ul class="menu vertical nested is-active">
+            <li class="menu-text">
+                <a href="html3">HTML</a>, <a href="translations/PL/py4e-pl-latest.pdf" target="_blank">PDF</a>, <a href="translations/PL/py4e-pl-latest.epub" target="_blank">EPUB</a>, <a href="translations/PL/py4e-pl-latest.mobi" target="_blank">MOBI</a>
+            </li>
+            <li class="menu-text"><a href="https://github.com/andre-wojtowicz/py4e-pl" target="_blank">Kod źródłowy książki na GitHubie</a> (zmiany w repozytorium powodują automatyczne przebudowanie ww. formatów na tej stronie)</li>
+        </ul>
+        <p>Współtwórcy: <a href="https://github.com/andre-wojtowicz" target="_blank">Andrzej Wójtowicz</a></p>
+    </li>
+    <li>
+        <a href="https://www.py4e.com/book">Inne języki</a>
+    </li>
 </ul>
 
+<p>Przykładowe kody programów oraz pliki danych do książki znajdują się <a href="code3" target="_blank">tutaj</a>.</p>
 <p>
-If you are insterested in starting a translation of the book, I have some
-instructions for 
-<a href="https://github.com/csev/py4e/blob/master/TRANSLATION.md"
- target="_blank">
-getting started</a>.
+Rozdziały 2-10 są mocno zaczerpnięte z otwartej książki zatytułowanej: "<a href="https://www.greenteapress.com/thinkpython/thinkCSpy/" target="_blank">Think Python: How to Think like a Computer Scientist</a>" autorstwa
+    <a href="https://allendowney.com/" target="_blank">Allena B. Downey'a</a> i <a href="https://www.elkner.net/" target="_blank">Jeffa Elknera</a>.
 </p>
 
-        <p>
-            The sample code and data files for the book is here: <a href="http://www.py4e.com/code3/" target="_blank">Code Samples</a>.
-        </p>
-        <p>
-            Chapters 2-10 are heavily adapted from the open book titled: "<a href="http://www.greenteapress.com/thinkpython/thinkCSpy/" target="_blank">Think Python: How to Think like a Computer Scientist</a>" by <a href="http://allendowney.com/" target="_blank">Allen B. Downey</a>
-and <a href="http://www.elkner.net/" target="_blank">Jeff Elkner</a>.
-        </p>
 <?php
 include("footer.php");
 ?>
