a96f293669dcb63b5397037377a8e9c0ba520c59
Andrzej Wójtowicz <andre@amu.edu.pl>
2020-10-01 11:07:43 +0200

    Translated privacy

--- privacy.php
+++ privacy.php
@@ -1,73 +1,70 @@
 <?php include("top.php");?>
 <?php include("nav.php");?>
-        <h2>Privacy Policy</h2>
-<p>This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
-<p><strong>What personal information do we collect from the people that visit our blog, website or app?</strong></p>
-<p>When ordering or registering on our site, as appropriate, you may be asked to enter your name, email address or other details to help you with your experience.</p>
-<p><strong>When do we collect information?</strong></p>
-<p>We may use the information we collect from you when you register, sign up for our newsletter, respond to a survey or marketing communication, surf the website, or use certain other site features.</p>
-<p><strong>How do we protect your information?</strong></p>
-<p>Your personal information is contained behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. In addition, all sensitive/credit information you supply is encrypted via Secure Socket Layer (SSL) technology.</p>
-<p><strong>Do we use 'cookies'?</strong></p>
-<p>Yes. Cookies are small files that a site or its service provider transfers to your computer's hard drive through your Web browser (if you allow) that enables the site's or service provider's systems to recognize your browser and capture and remember certain information.</p>
-<p><strong>We use cookies to:</strong></p>
-<p>Understand and save user's preferences and identity for future visits.</p>
-<p>Gather analytics data that is only used to monitor and improve the site.</p>
-<p><strong>If users disable cookies in their browser:</strong></p>
-<p>If you turn cookies off, you won't be able to log in to the site directly.</p>
-<p>You <b>can</b> use the learning tools on this system when launched from an LMS with cookies turned off since
-the learning applications <i>neither use nor require cookies</i> and will have full function even when cookies are turned off.
+        <h2>Polityka prywatności</h2>
+<p>Niniejsza polityka prywatności została opracowana aby lepiej służyć tym, którzy są zainteresowani tym jak ich "Dane osobowe" (w rozumieniu Personally Identifiable Information, PII) są wykorzystywane online. PII, zgodnie z amerykańskim prawem o ochronie prywatności i bezpieczeństwie informacji, to informacje, które mogą być wykorzystane samodzielnie lub wraz z innymi informacjami w celu identyfikacji, skontaktowania się lub zlokalizowania pojedynczej osoby lub też w celu identyfikacji osoby w zadanym kontekście. Prosimy o dokładne zapoznanie się z naszą polityką prywatności w celu uzyskania jasnego zrozumienia sposobu w jaki zbieramy, wykorzystujemy, chronimy lub w inny sposób posługujemy się informacjami umożliwiającymi identyfikację użytkownika zgodnie z naszą stroną internetową.</p>
+<p><strong>Jakie dane osobowe zbieramy od osób, które odwiedzają nasz blog, stronę internetową lub aplikację?</strong></p>
+<p>Przy zamawianiu lub rejestracji na naszej stronie internetowej, w zależności od potrzeb, możesz zostać poproszony o podanie swojego imienia i nazwiska, adresu e-mail lub innych danych, które pomogą Ci w przyswajaniu treści i realizacji zadań umieszczonych na tej stronie.</p>
+<p><strong>Kiedy zbieramy informacje?</strong></p>
+<p>Możemy korzystać z informacji zebranych od Ciebie podczas rejestracji, zapisania się do naszego newslettera, odpowiadania na ankietę lub komunikację marketingową, przeglądania strony internetowej lub korzystania z niektórych innych funkcji witryny internetowej.</p>
+<p><strong>Jak chronimy Twoje informacje?</strong></p>
+<p>Twoje dane osobowe znajdują się za zabezpieczonymi sieciami i są dostępne tylko dla ograniczonej liczby osób, które mają specjalne prawa dostępu do takich systemów i są zobowiązane do zachowania poufności tych informacji. Ponadto, wszystkie poufne/kredytowe informacje, które dostarczasz, są szyfrowane za pomocą technologii Secure Socket Layer (SSL).</p>
+<p><strong>Czy używamy "ciasteczek"?</strong></p>
+<p>Tak. Pliki ciasteczek są małymi plikami, które witryna lub jej dostawca usług przesyła na dysk twardy komputera użytkownika za pośrednictwem przeglądarki internetowej (jeśli użytkownik na to pozwoli), która umożliwia systemom witryny lub dostawcy usług rozpoznanie przeglądarki i przechwycenie oraz zapamiętanie pewnych informacji.</p>
+<p><strong>Używamy ciasteczek do:</strong></p>
+<p>Zrozumienia i zapisania preferencji i tożsamości użytkownika na potrzeby przyszłych wizyt.</p>
+<p>Zbierania danych analitycznych, które są wykorzystywane tylko do monitorowania i ulepszania strony.</p>
+<p><strong>Jeśli użytkownik wyłączy ciasteczka w swojej przeglądarce:</strong></p>
+<p>Jeśli wyłączysz pliki ciasteczek, nie będziesz mógł zalogować się bezpośrednio na stronie.</p>
+<p><b>Możesz</b> używać narzędzi edukacyjnych w tym systemie po uruchomieniu z systemu LMS z wyłączonymi plikami ciasteczek, ponieważ
+aplikacje edukacyjne <i>nie używają ani nie wymagają plików ciasteczek</i> i będą miały pełną funkcjonalność nawet po wyłączeniu plików ciasteczek.
 </p>
-<p><strong>Third-party disclosure</strong></p>
-<p>We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information.</p>
-<p><strong>Third-party links</strong></p>
-<p>We do not include or offer third-party products or services on our website.</p>
-<p><strong>Advertising</strong></p>
-<p>We do no third-party advertising on this site.</p>
+<p><strong>Ujawnianie informacji osobom trzecim</strong></p>
+<p>Nie sprzedajemy, nie wymieniamy ani w żaden inny sposób nie przekazujemy stronom trzecim Danych Osobowych (PII).</p>
+<p><strong>Powiązania z osobami trzecimi</strong></p>
+<p>Nie zamieszczamy ani nie oferujemy produktów ani usług osób trzecich na naszej stronie internetowej.</p>
+<p><strong>Reklama</strong></p>
+<p>Nie umieszczamy żadnych reklam osób trzecich na tej stronie.</p>
 <p><strong>Google Analytics</strong></p>
 <?php if ( isset($CFG->universal_analytics) && $CFG->universal_analytics ) { ?>
-<p>We use Google analytics to track usage and improve the site unless
-you request not to be tracked using a Do Not Track signal (see below).</p>
-<p>Google, as a third-party vendor, uses cookies to support analytics on our site. Users may opt-out of the use of the DART cookie by using the Google-provided opt-out capabilities. If users opt-out of Google Analytics on this site, there is no reduction in functionality of this site.</p>
+<p>Używamy Google Analytics do śledzenia korzystania z witryny i ulepszania jej, chyba że użytkownik zażąda, aby nie być śledzonym za pomocą sygnału "Do Not Track" (patrz poniżej).</p>
+<p>Google, jako dostawca zewnętrzny, używa plików ciasteczek do obsługi analiz na naszej stronie. Użytkownicy mogą zrezygnować z korzystania z plików ciasteczek DART, korzystając z możliwości rezygnacji udostępnianych przez Google. Jeśli użytkownicy zrezygnują z korzystania z Google Analytics na tej stronie, nie ograniczy to funkcjonalności tej strony.</p>
 <?php } else { ?>
-<p>We do not use Google Analytics on this site.</p>
+<p>Nie korzystamy z Google Analytics na tej stronie.</p>
 <?php } ?>
 <?php if ( isset($CFG->google_translate) && $CFG->google_translate ) { ?>
 <p><strong>Google Translate</strong></p>
-<p>We use Google Translate on this site unless
-you request not to be tracked using a Do Not Track signal (see below).</p>
+<p>Używamy Google Translate na tej stronie, chyba że użytkownik zażąda, aby nie być śledzonym za pomocą sygnału "Do Not Track" (patrz poniżej).</p>
 <?php } ?>
 <?php if ( isset($CFG->google_map_api_key) && $CFG->google_map_api_key ) { ?>
 <p><strong>Google Maps</strong></p>
-<p>Some pages on this site may use Google Maps.</p>
+<p>Niektóre strony na tej witrynie mogą korzystać z Google Maps.</p>
 <?php } ?>
-<p><strong>How does our site handle Do Not Track signals?</strong></p>
-<p>We do honor Do Not Track signals.  When DNT is indicated, we supress the gathering of all internal analytics data and do not use any third-party (Google only) analytics on the pages the user visits.</p>
+<p><strong>Jak nasza strona obsługuje sygnały "Do Not Track"?</strong></p>
+<p>Szanujemy sygnały "Do Not Track" (DNT). Kiedy otrzymujemy DNT, wyłączamy zbieranie wszystkich wewnętrznych danych analitycznych i nie używamy żadnych zewnętrznych (w naszym przypadku Google) danych analitycznych na stronach, które użytkownik odwiedza.</p>
 <p><strong>COPPA (Children Online Privacy Protection Act)</strong></p>
-<p>We do not specifically market to children under the age of 13 years old.</p>
-<p><strong>Data Retention</strong></p>
-<p>
-We actively expire inactive data based on a schedule. When data expires or is manually removed, this service does not retain a copy of the data. By only keeping active data in the system, we reduce the overall amount of 'Personally Identifiable Information' (PII) on the system at any time.
+<p>Nie adresujemy oferty w żaden szczególny sposób dla dzieci poniżej 13 roku życia.</p>
+<p><strong>Przechowywanie danych</strong></p>
+<p>Aktywnie wygasamy nieaktywne dane w oparciu o cykliczny harmonogram. Gdy dane wygasają lub są usuwane ręcznie, usługa ta nie zachowuje ich kopii. Przechowując tylko aktywne dane w systemie, zmniejszamy w każdej chwili ogólną ilość "Danych osobowych" (PII) w systemie.
 </p>
-<p><strong>Data Analysis</strong></p>
+<p><strong>Analiza danych</strong></p>
 <p>
-We will limit our analysis of PII data to reports requested by the customer or reports that are necessary for accounting, technical support, and overall statistics for the system. We may do internal data analysis to improve the technical reliability, functionality, or performance of the system. In particular we will not use Machine Learning, or Artificial Intelligence techniques to understand the behavior of those using our site.
+Ograniczymy naszą analizę "Danych osobowych" (PII) do raportów wymaganych przez klienta lub raportów, które są niezbędne do prowadzenia księgowości, wsparcia technicznego i ogólnych statystyk dla systemu. Możemy przeprowadzić wewnętrzną analizę danych w celu poprawy niezawodności technicznej, funkcjonalności lub wydajności systemu. W szczególności nie będziemy korzystać z technik uczenia maszynowego ani technik sztucznej inteligencji w celu zrozumienia zachowań osób korzystających z naszej strony.
 </p>
-<p><strong>General Philosphy</strong></p>
+<p><strong>Ogólna filozofia</strong></p>
 <p>
-It is often said that if you are not paying for a service, then you are the product. For this service, you are not the product and your data/activity is not the product. We retain only as much of your data as is needed to accomplish your teaching and learning goals and for no other purpose. Wherever possible, we give you direct control over the retention of your data and respect your wishes with respect to the retention of your data.
+Często mówi się, że jeśli nie płacisz za usługę, to jesteś produktem. Dla tej usługi nie jesteś produktem, a Twoje dane/aktywność nie jest produktem. Zatrzymujemy tylko tyle Twoich danych, ile jest potrzebne do osiągnięcia Twoich celów dydaktycznych i edukacyjnych i w żadnym innym celu. W miarę możliwości dajemy Tobie bezpośrednią kontrolę nad przechowywaniem Twoich danych i szanujemy Twoje oczekiwania w zakresie przechowywania danych.
 </p><p>
-Our goal is to comply both with the letter and spirit or privacy laws like the The Family Educational Rights and Privacy Act (FERPA) and EU General Data Protection Regulation (GDPR) regulations. We aspire to a company that far exceeds the minimum requirements of these laws.
+Naszym celem jest zapewnienie zgodności zarówno z literą jak i duchem prawa oraz przepisami o ochronie prywatności, takimi jak The Family Educational Rights and Privacy Act (FERPA) oraz Ogólnym rozporządzeniu o ochronie danych (RODO). Dążymy do tego, aby firma znacznie przekraczała minimalne wymagania tych przepisów.
 </p><p>
-Much of the software used in this company is open source so you can examine/audit what we are doing with respect to privacy and potentially help us improve our approaches to this very important topic.
+Duża część oprogramowania używanego w tej firmie jest oprogramowaniem otwartoźródłowym, więc możesz zbadać/audytować to, co robimy w odniesieniu do prywatności i potencjalnie pomóc nam poprawić nasze podejście do tego bardzo ważnego tematu.
 </p>
-<p><strong>Notifications</strong> </p>
-<p>In order to be in line with Fair Information Practices we will take the following responsive action, should a data breach occur: We will notify you via email</p>
-<p>If there are any questions regarding this privacy policy, you may contact us using the information below.<br></p>
+<p><strong>Powiadomienia</strong> </p>
+<p>W celu zachowania zgodności z uczciwymi praktykami informacyjnymi (w znaczeniu fair information practice, FIP) podejmiemy następujące działania w przypadku naruszenia danych: powiadomimy Ciebie za pośrednictwem poczty elektronicznej.</p>
+<p>W przypadku jakichkolwiek pytań dotyczących niniejszej polityki prywatności, możesz skontaktować się z nami korzystając z poniższych informacji.<br></p>
 <pre><code>Python for Everybody (py4e.com)
 1537 Holbrook Drive
 Holt, Michigan 48842
 United States</code></pre>
-<p>Last edited: 27-Nov-2017</p>
+<p>Ostatnia edycja: 27-11-2017</p>
 <?php include("footer.php"); ?>
 
