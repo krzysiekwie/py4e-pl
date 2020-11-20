5c7b635be1427175dbff5ccab42a1f866d7d9ff0
krzwierz79 <krzwierz79@gmail.com>
2020-11-20 17:55:36 +0100

    revision-2

--- code3/hidden.py
+++ code3/hidden.py
@@ -8,9 +8,9 @@
 #    b) kliknij na "Apply"
 #    c) wybierz "Hobbyist" > "Exploring the API" i wybierz "Next"
 #    d) uzupełnij formularz (może być wymagana weryfikacja numeru telefonu)
-#    e) w kolejnym kroku opisz po angielsku jak zamierzasz wykorzystać dane
+#    e) w kolejnym kroku opisz po angielsku, jak zamierzasz wykorzystać dane
 #       Twittera (wspomnij, że uczysz się Pythona, chcesz dowiedzieć
-#       się jak korzystać z API Twittera, zamierzasz napisać dwa mini-projekty,
+#       się, jak korzystać z API Twittera, zamierzasz napisać dwa mini-projekty,
 #       które w interaktywny sposób pobierają nazwę konta Twittera i dla podanego
 #       konta w projekcie nr 1 pobierzesz i wyświetlisz oś czasu użytkownika
 #       zwróconą w formacie JSON, a w projekcie nr 2 pobierzesz listę jego
@@ -19,14 +19,14 @@
 #       na akceptację Twojego zgłoszenia); w sekcji "Specifics" zaznacz wszystko
 #       na "No"
 #    f) zweryfikuj zgłoszenie, przeczytaj warunki umowy i wyślij zgłoszenie
-#    g) potwierdź zgłoszenie klikając na link w wiadomości mailowej,
+#    g) potwierdź zgłoszenie, klikając na link w wiadomości mailowej,
 #       którą otrzymasz po wysłaniu zgłoszenia
 # 3. Gdy Twoje zgłoszenie zostanie zaakceptowane, wejdź jeszcze raz
 #    na https://developer.twitter.com/en/apps i ponownie spróbuj
 #    utworzyć nową aplikację ("Standalone Apps" > "Create App")
 # 4. Utwórz aplikację, w karcie "Keys and tokens" wygeneruj klucze i podmień
 #    poniżej cztery ciągi znaków dotyczące consumer_key, consumer_secret,
-#    token_key i token_secret (podmień wszystko co znajduje się między
+#    token_key i token_secret (podmień wszystko, co znajduje się między
 #    cudzysłowami, łącznie z nawiasami ostrokątnymi)
 
 def oauth():
