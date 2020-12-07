7cac2327e481fd7225807ce31444dbcd4cc61e4d
Andrzej Wójtowicz <andre@amu.edu.pl>
2020-12-07 18:12:00 +0100

    Updated formats

--- book3/book.sh
+++ book3/book.sh
@@ -15,14 +15,14 @@ fi
 
 # cleanup
 
-rm tmp-1.* tmp-2.* tmp-3.* *.tmp *.aux 2> /dev/null
+rm tmp-1.* tmp-2.* tmp-3.* tmp-4.* *.tmp *.aux 2> /dev/null
 
 # PDF print color version
 
 pandoc <(sed "s/^Copyright 2009– /Copyright 2009–$(date +%Y) /" A0-preface.mkd) -o tmp-1.prefacex.tex
 sed < tmp-1.prefacex.tex 's/section{/section*{/' > tmp-1.preface.tex
 
-cat [0-9]*.mkd | python2 verbatim.py | tee tmp-1.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V pdfversionprint -V fontsize:10pt -V documentclass:book -V lang:pl-PL -V langbabel:polish -V "author:Dr Charles R. Severance" -V "title:Python dla wszystkich" -V "subtitle:Odkrywanie danych z Python 3" -V colorlinks:false -V citecolor:black -V urlcolor:black -V linkcolor:black -V numbersections --syntax-definition=sql.xml --template=template.latex -o tmp-1.tex
+cat [0-9]*.mkd | python2 verbatim.py | tee tmp-1.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V pdfversionprint -V fontsize:10pt -V documentclass:book -V lang:pl-PL -V langbabel:polish -V "author:Dr Charles R. Severance" -V "title:Python dla wszystkich" -V "subtitle:Odkrywanie danych z Python 3" -V colorlinks:false -V citecolor:black -V urlcolor:black -V linkcolor:black -V numbersections -V typeinclude_t1 --syntax-definition=sql.xml --template=template.latex -o tmp-1.tex
 pandoc [A-Z][A-Z]*.mkd -o tmp-1.app.tex
 
 sed < tmp-1.app.tex -e 's/subsubsection{/xyzzy{/' -e 's/subsection{/plugh{/' -e 's/section{/chapter{/' -e 's/xyzzy{/subsection{/' -e 's/plugh{/section{/'  > tmp-1.appendix.tex
@@ -36,18 +36,20 @@ perl -0777 -i -pe "s/\`\`(.+?)''/\\\\enquote{\1}/igs" tmp-1.appendix.tex
 
 mv tmp-1.patch tmp-1.tex
 
+perl -0777 -i -pe "s/\\\\href\\{(.+?)\\}\\{(.+?)\\}/\\\\href{\1}{\\\\nolinkurl{\2}}/igs" tmp-1.tex
+
 pdflatex -shell-escape tmp-1.tex # first, TOC
 pdflatex -shell-escape tmp-1.tex # second, add TOC
 mv tmp-1.pdf x-1.pdf
 
-# PDF A4
+# PDF print A4
 
 pandoc <(sed "s/^Copyright 2009– /Copyright 2009–$(date +%Y) /" A0-preface.mkd) -o tmp-2.prefacex.tex
 sed < tmp-2.prefacex.tex 's/section{/section*{/' > tmp-2.preface.tex
 
 cp 01-intro.mkd 01-intro.mkd.orig
 sed -i "3i% Ostatnia aktualizacja: $(date '+%Y-%m-%d')" 01-intro.mkd
-cat [0-9]*.mkd | python2 verbatim.py | tee tmp-2.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V pdfversiona4  -V fontsize:10pt -V documentclass:book -V papersize:a4paper -V openany -V lang:pl-PL -V langbabel:polish -V "author:Dr Charles R. Severance" -V "title:Python dla wszystkich" -V "subtitle:Odkrywanie danych z Python 3"  -V numbersections --template=template.latex --syntax-definition=sql.xml -o tmp-2.tex
+cat [0-9]*.mkd | python2 verbatim.py | tee tmp-2.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V pdfversiona4  -V fontsize:10pt -V documentclass:book -V papersize:a4paper -V openany -V lang:pl-PL -V langbabel:polish -V "author:Dr Charles R. Severance" -V "title:Python dla wszystkich" -V "subtitle:Odkrywanie danych z Python 3"  -V numbersections -V typeinclude_t2 --template=template.latex --syntax-definition=sql.xml -o tmp-2.tex
 pandoc [A-Z][A-Z]*.mkd -o tmp-2.app.tex
 mv 01-intro.mkd.orig 01-intro.mkd
 
@@ -62,6 +64,8 @@ perl -0777 -i -pe "s/\`\`(.+?)''/\\\\enquote{\1}/igs" tmp-2.appendix.tex
 
 mv tmp-2.patch tmp-2.tex
 
+perl -0777 -i -pe "s/\\\\href\\{(.+?)\\}\\{(.+?)\\}/\\\\href{\1}{\\\\nolinkurl{\2}}/igs" tmp-2.tex
+
 pdflatex -shell-escape tmp-2.tex # first, TOC
 pdflatex -shell-escape tmp-2.tex # second, add TOC
 mv tmp-2.pdf x-2.pdf
@@ -71,7 +75,7 @@ mv tmp-2.pdf x-2.pdf
 pandoc <(sed "s/^Copyright 2009– /Copyright 2009–$(date +%Y) /" A0-preface.mkd) -o tmp-3.prefacex.tex
 sed < tmp-3.prefacex.tex 's/section{/section*{/' > tmp-3.preface.tex
 
-cat [0-9]*.mkd | python2 verbatim.py | tee tmp-3.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V pdfversionprint -V fontsize:10pt -V documentclass:book -V lang:pl-PL -V langbabel:polish -V "author:Dr Charles R. Severance" -V "title:Python dla wszystkich" -V "subtitle:Odkrywanie danych z Python 3" -V colorlinks:false -V citecolor:black -V urlcolor:black -V linkcolor:black -V numbersections --highlight-style=monochrome --template=template.latex --syntax-definition=sql.xml -o tmp-3.tex
+cat [0-9]*.mkd | python2 verbatim.py | tee tmp-3.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V pdfversionprint -V fontsize:10pt -V documentclass:book -V lang:pl-PL -V langbabel:polish -V "author:Dr Charles R. Severance" -V "title:Python dla wszystkich" -V "subtitle:Odkrywanie danych z Python 3" -V colorlinks:false -V citecolor:black -V urlcolor:black -V linkcolor:black -V numbersections --highlight-style=monochrome  -V typeinclude_t3 --template=template.latex --syntax-definition=sql.xml -o tmp-3.tex
 pandoc [A-Z][A-Z]*.mkd -o tmp-3.app.tex
 
 sed < tmp-3.app.tex -e 's/subsubsection{/xyzzy{/' -e 's/subsection{/plugh{/' -e 's/section{/chapter{/' -e 's/xyzzy{/subsection{/' -e 's/plugh{/section{/'  > tmp-3.appendix.tex
@@ -85,11 +89,41 @@ perl -0777 -i -pe "s/\`\`(.+?)''/\\\\enquote{\1}/igs" tmp-3.appendix.tex
 
 mv tmp-3.patch tmp-3.tex
 
+perl -0777 -i -pe "s/\\\\href\\{(.+?)\\}\\{(.+?)\\}/\\\\href{\1}{\\\\nolinkurl{\2}}/igs" tmp-3.tex
+
 pdflatex -shell-escape tmp-3.tex # first, TOC
 pdflatex -shell-escape tmp-3.tex # second, add TOC
 mv tmp-3.pdf x-3.pdf
 
+# PDF electronic A4
+
+pandoc <(sed "s/^Copyright 2009– /Copyright 2009–$(date +%Y) /" A0-preface.mkd) -o tmp-4.prefacex.tex
+sed < tmp-4.prefacex.tex 's/section{/section*{/' > tmp-4.preface.tex
+
+cp 01-intro.mkd 01-intro.mkd.orig
+sed -i "3i% Ostatnia aktualizacja: $(date '+%Y-%m-%d')" 01-intro.mkd
+cat [0-9]*.mkd | python2 verbatim.py | tee tmp-4.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V pdfversiona4  -V fontsize:10pt -V documentclass:book -V papersize:a4paper -V openany -V lang:pl-PL -V langbabel:polish -V "author:Dr Charles R. Severance" -V "title:Python dla wszystkich" -V "subtitle:Odkrywanie danych z Python 3"  -V numbersections -V fontcode:lmss  -V typeinclude_t4 --template=template.latex --syntax-definition=sql.xml -o tmp-4.tex
+pandoc [A-Z][A-Z]*.mkd -o tmp-4.app.tex
+mv 01-intro.mkd.orig 01-intro.mkd
+
+sed < tmp-4.app.tex -e 's/subsubsection{/xyzzy{/' -e 's/subsection{/plugh{/' -e 's/section{/chapter{/' -e 's/xyzzy{/subsection{/' -e 's/plugh{/section{/'  > tmp-4.appendix.tex
+
+sed < tmp-4.tex '/includegraphics/s/jpg/eps/' | sed 's"includegraphics{../photos"includegraphics[height=3.0in]{../photos"' > tmp-4.sed
+diff tmp-4.sed tmp-4.tex
+python2 texpatch.py < tmp-4.sed > tmp-4.patch
+
+perl -0777 -i -pe "s/\`\`(.+?)''/\\\\enquote{\1}/igs" tmp-4.preface.tex
+perl -0777 -i -pe "s/\`\`(.+?)''/\\\\enquote{\1}/igs" tmp-4.appendix.tex
+
+mv tmp-4.patch tmp-4.tex
+
+perl -0777 -i -pe "s/\\\\href\\{(.+?)\\}\\{(.+?)\\}/\\\\href{\1}{\\\\nolinkurl{\2}}/igs" tmp-4.tex
+
+pdflatex -shell-escape tmp-4.tex # first, TOC
+pdflatex -shell-escape tmp-4.tex # second, add TOC
+mv tmp-4.pdf x-4.pdf
+
 # cleanup
 
 rm ../images/*-eps-converted-to.pdf
-rm tmp-1.* tmp-2.* tmp-3.*
+rm tmp-1.* tmp-2.* tmp-3.* tmp-4.*
