c1d15087b910c088afc094cfacdd6157cf2772db
Andrzej Wójtowicz <andre@amu.edu.pl>
2020-12-07 18:15:00 +0100

    Updated book webpage

--- book.php
+++ book.php
@@ -11,7 +11,10 @@
         <ul class="menu vertical nested is-active">
             <li class="menu-text">ostatnia aktualizacja: <?php exec('git log -1 --format="%at" | xargs -I{} date -d @{} "+%Y-%m-%d"', $commit_date); echo($commit_date[0]); ?></li>
             <li class="menu-text">
-                <a href="html3">HTML</a>, <a href="translations/PL/py4e-pl-a4-latest.pdf" target="_blank">PDF (format A4)</a>, <a href="translations/PL/py4e-pl-print-latest.pdf" target="_blank">PDF (format do druku w kolorze)</a>, <a href="translations/PL/py4e-pl-print-bw-latest.pdf" target="_blank">PDF (format do druku czarno-białego)</a>, <a href="translations/PL/py4e-pl-latest.epub" target="_blank">EPUB</a>, <a href="translations/PL/py4e-pl-latest.mobi" target="_blank">MOBI</a>
+                do czytania na ekranie: <a href="html3">HTML</a>, <a href="translations/PL/py4e-pl-a4-online-latest.pdf" target="_blank">PDF</a>, <a href="translations/PL/py4e-pl-latest.epub" target="_blank">EPUB</a>, <a href="translations/PL/py4e-pl-latest.mobi" target="_blank">MOBI</a>
+            </li>
+            <li class="menu-text">
+                do druku (PDF): <a href="translations/PL/py4e-pl-a4-latest.pdf" target="_blank">A4 kolor</a>, <a href="translations/PL/py4e-pl-print-latest.pdf" target="_blank">7 × 10 " kolor</a>, <a href="translations/PL/py4e-pl-print-bw-latest.pdf" target="_blank">7 × 10 " czarno-biały</a>
             </li>
             <li class="menu-text"><a href="https://github.com/andre-wojtowicz/py4e-pl" target="_blank">Kod źródłowy książki na GitHubie</a> (zmiany w repozytorium powodują automatyczne przebudowanie ww. formatów na tej stronie)</li>
         </ul>
